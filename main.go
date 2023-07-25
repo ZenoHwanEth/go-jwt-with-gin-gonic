f92dbbd8bcccaf12492e1ca2721f62846d7e38e8
ZenoHwanEth <hwan2221@hotmail.com>
2023-07-25 18:25:01 +0800

    adding routes in main.go

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"os"
 
+	routes "github.com/ZenoHwanEth/go-jwt-with-gin-gonic/routes"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,4 +16,18 @@ func main() {
 	}
 
 	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-1"})
+	})
+
+	router.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+
+	router.Run(":" + port)
 }
