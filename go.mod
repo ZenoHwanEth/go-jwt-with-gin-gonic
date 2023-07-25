b8f3912798c01580c37d8f589b806ae422eb565f
ZenoHwanEth <hwan2221@hotmail.com>
2023-07-25 09:17:06 +0800

    add type user struct

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ZenoHwanEth/go-jwt-with-gin-gonic
 
 go 1.20
+
+require go.mongodb.org/mongo-driver v1.12.0 // indirect
--- models/useModel.go
+++ models/useModel.go
@@ -1,1 +1,22 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID            primitive.ObjectID `bson:"_id"`
+	First_name    *string            `json:"first_name" validate:"required, min=2, max=100"`
+	Last_name     *string
+	Password      *string
+	Email         *string
+	Phone         *string
+	Token         *string
+	User_Type     *string
+	Refresh_token *string
+	Created_at    time.Time
+	Updated_at    time.Time
+	User_id       string
+}
