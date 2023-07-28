54ebf05cc9a6ade565d2d4621bd49e09e6764bc1
ZenoHwanEth <hwan2221@hotmail.com>
2023-07-28 23:13:21 +0800

    adding function template for  userController

---
+++ controllers/userController.go
@@ -0,0 +1,13 @@
+package controllers
+
+func HashPassword(password string)
+
+func VerifyPassword(password string)
+
+func Signup(password string)
+
+func Login(password string)
+
+func GetUsers()
+
+func GetUser()
