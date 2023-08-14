09dd8a0976e8b153c79a8169e94345be7a5e7e2c
ZenoHwanEth <hwan2221@hotmail.com>
2023-08-14 21:33:38 +0800

    adding jwt "github.com/dgrijalva/jwt-go" and userCollection

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,8 +1,18 @@
 package helper
 
+import (
+	"github.com/ZenoHwanEth/go-jwt-with-gin-gonic/database"
+	jwt "github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 type SignedDetails struct {
 	Email      string
 	First_name string
 	Last_name  string
 	Uid        string
+	User_type  string
+	jwt.StandardClaims
 }
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
